2f61b8513649da018215f7d72cbede6dc7255f3f
Alexis V <definealexis@gmail.com>
2022-03-08 17:52:31 -0600

    m1

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">¿En que estas pensando ahora?</div>
+
+            <div class="card-body">
+
+                <form action="" v-on:submit.prevent="newThought()">
+                    <div class="form-group">
+                        <label for="thought">Sera que estoy pensando en: </label>
+                        <input type="text" class="form-control" name="thought"
+                            v-model="description">
+                    </div>
+                    <button type="submit" class="btn btn-primary">
+                        Enviar pensamiento
+                    </button>
+                </form>    
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                description: ''
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        methods: {
+            newThought() {
+                let thought = {
+                    id: 2,
+                    description: this.description,
+                    created_at: '11/07/2011'
+                };
+
+                this.$emit('new', thought);
+                this.description = '';
+            }
+        }
+    }
+</script>
---
+++ resources/js/components/MyThoughtsComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row justify-content-center">
+
+            <form-component @new="addThought"></form-component>
+
+            <thought-component 
+                v-for="(thought, index) in thoughts"
+                :key="thought.id"
+                :thought="thought"
+                @update="updateThought(index, ...arguments)"
+                @delete="deleteThought(index)">
+            </thought-component>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                thoughts: [{
+                    'id': 1,
+                    'description': 'esta es la descripcion',
+                    'created_at': '17/03/2022'
+                }]
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        methods: {
+            addThought(thought) {
+                this.thoughts.push(thought);
+            },
+            updateThought(index, thought) {
+                this.thoughts[index] = thought;
+            },
+            deleteThought(index) {
+                this.thoughts.slice(index, 1);
+            }
+        }
+    }
+</script>
